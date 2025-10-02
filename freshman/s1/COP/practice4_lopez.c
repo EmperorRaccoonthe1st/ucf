92c0c5f8206ed2403db9293eabe972e3db1aa35b
EmperorRaccoonthe1st <owenmlopez07@gmail.com>
2025-10-02 15:11:55 -0400

    started practice 4 fr

--- freshman/s1/COP/practice4_lopez.c
+++ freshman/s1/COP/practice4_lopez.c
@@ -4,10 +4,19 @@ int get_reverse(int num);
 int is_palindrome(int num);
 
 int main(void) {
-
+    int input;
+    printf("Input a number here:");
+    scanf("%d", &input);
+    get_reverse(input);
     return 0;
 }
 
 int get_reverse(int num) {
-    
+    int r;
+    while ((num/10) > 0) {
+        r += (num % (num/10))*(num/10);
+        printf("%d\n", r);
+        num = num/10;
+    }
+    return r;
 }
